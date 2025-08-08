1ddc0ff65d852af93b611c68ffeafff9b0f91024
EduardoPacheco101 <eduardoalvespacheco2005@gmail.com>
2025-08-07 22:24:53 -0300

    Add files via upload

---
+++ Untitled-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+printf("Eduardo, 19 anos, Licenciatura em Computação.");
+printf("\nTexto formatado");
+int x = 0 ;
+for (x = 3; x < 30; x + 3){
+    printf(x);
+    
+}
+return 0;
+
+}
