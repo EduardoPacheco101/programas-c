616768dc0becc7bcfaaf2ac233b705434e208641
EduardoPacheco101 <eduardoalvespacheco2005@gmail.com>
2025-08-13 20:34:51 -0300

    Add files via upload

---
+++ Etapa_2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+int main(void){
+
+    int num_1;
+    int num_2;
+    int soma;
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &num_1);
+    printf("Digite o segundo numero: ");
+    scanf("%d", &num_2);
+    printf("\nA soma e: %d", soma = num_1 + num_2);
+    printf("\nA subtracao e: %d", soma = num_1 - num_2);
+    printf("\nA multiplicacao e: %d", soma = num_1 * num_2);
+    printf("\nO resto e: %d", soma = num_1 % num_2);
+    printf("\nA divisao e: %d", soma = num_1 / num_2);
+    return 0;
+
+}
+*/
+
+int main(void){
+
+    return 0;
+}
