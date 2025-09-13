fd933178198f361990fb907f813f108442433161
EduardoPacheco101 <eduardoalvespacheco2005@gmail.com>
2025-09-13 16:34:51 -0300

    Add files via upload

---
+++ etapas_c/Etapa_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+printf("Eduardo, 19 anos, Licenciatura em Computação.");
+printf("\nTexto formatado\n");
+int x = 0;
+for(x = 0; x <= 30; x += 3){
+    printf("%d\n",x);
+    
+}
+return 0;
+
+}
